aa61a071ec1ae82cc4cbd478a446801421f51a0d
crlsktr <crlsktr@gmail.com>
2020-03-25 15:21:49 -0600

    angular stuff

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { CreateUserModule } from './create-user/create-user.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CreateUserModule
+    CreateUserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ui/src/app/create-user/new-user-form/new-user-form.component.ts
+++ ui/src/app/create-user/new-user-form/new-user-form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-new-user-form',
-  templateUrl: './new-user-form.component.html',
-  styleUrls: ['./new-user-form.component.scss']
+	selector: 'app-new-user-form',
+	templateUrl: './new-user-form.component.html',
+	styleUrls: ['./new-user-form.component.scss']
 })
 export class NewUserFormComponent implements OnInit {
 
-  constructor() { }
+	newUserForm :FormGroup = new FormGroup({
+		userName: new FormControl(''),
+		passPhrase: new FormControl('')
+	});
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+		this.newUserForm.controls.userName.valueChanges.subscribe( x => {
+			console.log("value", x);
+		});
+	}
 
 }
