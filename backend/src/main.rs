dde2939283c8fd5d4692670f1effd0dd08645961
crlsktr <crlsktr@gmail.com>
2025-05-02 14:01:03 -0600

    allow communication between ui and backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,7 +5,13 @@ fn hello(name: &str, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
+#[get("/")]
+fn ping() -> String {
+    format!("hello")
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![hello])
+    rocket::build().mount("/hello", routes![hello]).mount("/ping", routes![ping])
+    
 }
--- dockerfile
+++
@@ -1,7 +0,0 @@
-FROM rust:slim
-WORKDIR /usr/src/gameshare
-COPY . .
-
-RUN cargo install --path .
-
-CMD ["boardgamesharing"]
